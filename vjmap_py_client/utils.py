787c6945c0e46988c47f2ba3c1c7f7f94bf24c00
wangying <wangying@xkool.org>
2024-10-31 16:26:58 +0800

    add api for file like object upload

--- vjmap_py_client/utils.py
+++ vjmap_py_client/utils.py
@@ -21,3 +21,20 @@ def file_md5(file_path: str) -> str:
         raise IOError(f"Error reading file: {e}")
 
 
+def file_object_md5(file_object) -> str:
+    """
+    获取文件对象的MD5值
+    :param file_object: 文件对象
+    :return: 文件的MD5哈希值
+    """
+    md5_hash = hashlib.md5()  # 创建MD5哈希对象
+    chunk_size = 2097152  # 2MB
+
+    try:
+        while chunk := file_object.read(chunk_size):    # 按块读取文件
+            md5_hash.update(chunk)  # 更新哈希对象
+
+        return md5_hash.hexdigest()  # 返回十六进制格式的MD5值
+
+    except Exception as e:
+        raise IOError(f"Error reading file: {e}")
